15ee45a84c7b4d198b9930b8141e54a0aca97c72
ghozimahdi <profghozimahdi@gmail.com>
2019-12-23 11:23:39 +0700

    sorting module

--- sorting.js
+++ sorting.js
@@ -1,4 +1,4 @@
-var arr = [9, 8, 7, 12, 1, 0];
+var arr = [50, 100, 20, 1, 3, 0];
 for (var i = 1; i < arr.length; i++) {
     var temp = arr[i];
     for (var j = i - 1; j >= 0 && temp < arr[j]; j--){
